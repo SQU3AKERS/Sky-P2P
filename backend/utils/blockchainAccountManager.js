b2c577e663708cbc266fc30ed699948e58d7aa35
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-17 02:05:34 +0800

    Create blockchainAccountManager.js

---
+++ backend/utils/blockchainAccountManager.js
@@ -0,0 +1,19 @@
+const Web3 = require('web3');
+const fs = require('fs');
+const path = require('path');
+
+const web3 = new Web3('http://localhost:7545');
+
+const createAccountAndStorePrivateKey = async () => {
+  const account = await web3.eth.accounts.create();
+
+  const privateKeyPath = path.resolve(__dirname, '../storage/privatekey-users', `privatekey-${account.address}.txt`);
+
+  fs.writeFileSync(privateKeyPath, account.privateKey);
+
+  return account.address;
+};
+
+module.exports = {
+  createAccountAndStorePrivateKey
+};
