b8a6adeb41fc676e08b6e99612bfe5cd877b6250
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-17 16:33:18 +0800

    Update userController.js
    Added console.log to debug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,13 +24,16 @@ const registerUser = async (req, res) => {
     database.query(insertQuery, [firstName, lastName, passwordHash, email, dateOfBirth, nationality, userType, blockchainAddress], (error, results) => {
       if (error) {
         // Handle SQL error
+        console.log("SQL error", error);
         return res.status(500).json({ message: "Error registering user", error: error.sqlMessage || error });
       }
       // Registration successful
+      console.log("Registration successful", results);
       res.status(201).json({ message: "User registered successfully", userId: results.insertId });
     });
   } catch (error) {
     // Handle server error
+    console.log("Server error", error);
     res.status(500).json({ message: "Server error", error: error.message || error });
   }
 };
