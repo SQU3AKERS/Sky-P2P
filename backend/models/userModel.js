e5070c72d36622f612cf2b15a02e1d4730ddfd71
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-18 00:58:18 +0800

    Added Models

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,45 +4,14 @@ const sequelize = require('../config/database');
 class User extends Model {}
 
 User.init({
-  UserID: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  FirstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  LastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  PasswordHash: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  Email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  DateOfBirth: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  Nationality: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  UserType: {
-    type: DataTypes.ENUM('Lender', 'Borrower'),
-    allowNull: false
-  },
-  // Include other fields as necessary
-}, {
-  sequelize,
-  modelName: 'User',
-  timestamps: false
-});
+  UserID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  FirstName: { type: DataTypes.STRING, allowNull: false },
+  LastName: { type: DataTypes.STRING, allowNull: false },
+  PasswordHash: { type: DataTypes.STRING, allowNull: false },
+  Email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  DateOfBirth: { type: DataTypes.DATE, allowNull: false },
+  Nationality: { type: DataTypes.STRING, allowNull: false },
+  UserType: { type: DataTypes.ENUM('Lender', 'Borrower'), allowNull: false },
+}, { sequelize, modelName: 'User', timestamps: false });
 
 module.exports = User;
