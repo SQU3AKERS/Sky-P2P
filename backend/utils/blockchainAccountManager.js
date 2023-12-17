b79d72b2c27cad0d47a4d455d4836fe896a13bc7
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-17 18:18:12 +0800

    Update blockchainAccountManager.js

--- backend/utils/blockchainAccountManager.js
+++ backend/utils/blockchainAccountManager.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 const fs = require('fs');
 const path = require('path');
 
-const web3 = new Web3('http://localhost:7545');
+const web3 = new Web3('http://127.0.0.1:7545');
 
 const createAccountAndStorePrivateKey = async () => {
   const account = await web3.eth.accounts.create();
