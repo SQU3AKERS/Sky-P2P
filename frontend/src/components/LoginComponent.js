5e6c001165ebd14550f50d7f05766d9d18db0bdc
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-17 21:55:16 +0800

    Update LoginComponent.js

--- frontend/src/components/LoginComponent.js
+++ frontend/src/components/LoginComponent.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from React Router
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 const LoginComponent = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    // Update state based on input type
+    const { id, value } = e.target;
+    if (id === 'email') setEmail(value);
+    if (id === 'password') setPassword(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    // Simple front-end validation
+    if (!email) {
+      alert('Please enter your email.');
+      return;
+    }
+    if (!password) {
+      alert('Please enter your password.');
+      return;
+    }
+    // Regex pattern for basic email validation
+    const emailPattern = /\S+@\S+\.\S+/;
+    if (!emailPattern.test(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+    const userType = 'borrower';
+    navigate(userType === 'borrower' ? '/borrower-mainpage' : '/lender-mainpage');
+  };
+
   return (
     <div className="login-component">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Login</h2>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="email" id="email" value={email} onChange={handleInputChange} placeholder="Email" />
+        <input type="password" id="password" value={password} onChange={handleInputChange} placeholder="Password" />
         <button type="submit">Log In</button>
+        {/* Other content */}
         <p className="register-link">Don't have an account?<br/><Link to="/register">Register New Account Here</Link></p>
       </form>
     </div>
