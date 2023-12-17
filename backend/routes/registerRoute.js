a7443e12c1cfa2abbb018b1e09b12e563bc738d8
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-17 17:04:14 +0800

    Removed redundancy in /register server route

--- backend/routes/registerRoute.js
+++ backend/routes/registerRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/registerController');
+const registerController = require('../controllers/registerController');
 
-router.post('/register', userController.registerUser);
+router.post('/register', registerController.registerUser);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,6 @@ app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json()); // Enable JSON body parsing
 app.use('/api/users', registerRoute);
 
-// Define a POST route for registration
-app.post('/register', (req, res) => {
-});
-
 // Start the server on port 3001, avoid conflict port 3000 with React
 app.listen(3001, () => {
   console.log('Server running on port 3001');
