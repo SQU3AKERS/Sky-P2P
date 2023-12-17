a8c40abefce13704bd2f2303c2030a29e4af2904
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-18 05:18:12 +0800

    Update loginController.js

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const connection = require('../config/database');
+const database = require('../config/database');
 
 const handleLogin = async (req, res) => {
   const { email, password } = req.body;
   console.log('Login request received for email:', email);
 
-  connection.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
+  database.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
     if (err) {
       console.log('Database error:', err);
       return res.status(500).send('Internal server error');
