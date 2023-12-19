c4e5b48358d4aaea14f49e7d7f873659aa656cf3
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-19 13:52:30 +0800

    Added Database Table Creation and Insertion Support

---
+++ backend/intiDatabase.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = require('./config/sequelize'); // Adjust the path as necessary
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+const initializeDatabase = async () => {
+  try {
+    // Path to your SQL files
+    const sqlDirectory = path.join(__dirname, 'storage', 'sql');
+
+    // Read all SQL files from the directory
+    const sqlFiles = fs.readdirSync(sqlDirectory).filter(file => file.endsWith('.sql'));
+
+    for (const file of sqlFiles) {
+      const filePath = path.join(sqlDirectory, file);
+      const sql = fs.readFileSync(filePath, 'utf8');
+      
+      // Execute the SQL file
+      await sequelize.query(sql);
+    }
+
+    console.log('Database initialized successfully');
+  } catch (error) {
+    console.error('Failed to initialize database:', error);
+  } finally {
+    await sequelize.close();
+  }
+};
+
+initializeDatabase();
