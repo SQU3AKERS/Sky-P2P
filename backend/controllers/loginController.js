81578e69b338b8ce373bb74dea3f2797138310f5
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-19 16:20:25 +0800

    Updated controllers
    To work with better session control and validation

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,27 +1,40 @@
-const User = require('../models/userModel');
+const User = require('../models/userModel'); // Adjust the path as needed
 const bcrypt = require('bcrypt');
-const { createSession } = require('../utils/sessionManager');
+const { createSession } = require('../utils/sessionManager'); // Adjust the path as needed
+const { validateEmail } = require('../utils/validation'); // Adjust the path as needed
 
 const loginController = {
   login: async (req, res) => {
     const { email, password } = req.body;
+
     try {
+      // Validate Email
+      if (!validateEmail(email)) {
+        return res.status(400).json({ message: "Invalid email format" });
+      }
+
+      // Check if user exists
       const user = await User.findOne({ where: { Email: email } });
       if (!user) {
-        console.log('No such user:', user);
-        return res.status(401).send('Invalid credentials');
+        console.log('Login attempt with non-existent email:', email);
+        return res.status(401).json({ message: "Invalid credentials" });
       }
+
+      // Compare password
       const match = await bcrypt.compare(password, user.PasswordHash);
       if (!match) {
-        console.log('Password mismatch:', match);
-        return res.status(401).send('Invalid credentials');
+        console.log('Password mismatch for user:', email);
+        return res.status(401).json({ message: "Invalid credentials" });
       }
+
+      // Create session
       const sessionId = createSession(user.UserID, user.UserType);
-      console.log('Session created:', sessionId);
+      console.log('Login successful, session created:', sessionId);
       res.json({ success: true, sessionId, userType: user.UserType });
+
     } catch (err) {
-      console.error('Login error:', err);
-      res.status(500).send('Server error');
+      console.error('Login error for user:', email, 'Error:', err);
+      res.status(500).json({ message: "Server error", error: err.message || err });
     }
   }
 };
--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
-const database = require('../config/database');
+const User = require('../models/userModel'); // Adjust the path as needed
+const { validateEmail, validatePassword } = require('../utils/validation'); // Adjust the path as needed
 
 const saltRounds = 10;
 
@@ -7,29 +8,35 @@ const registerUser = async (req, res) => {
   const { firstName, lastName, email, password, dateOfBirth, nationality, userType } = req.body;
 
   try {
+    // Validate Email and Password
+    if (!validateEmail(email) || !validatePassword(password)) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email already in use" });
+    }
+
     // Hash the password
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    // Prepare the SQL query
-    const insertQuery = `
-      INSERT INTO users (FirstName, LastName, PasswordHash, Email, DateOfBirth, Nationality, UserType)
-      VALUES (?, ?, ?, ?, ?, ?, ?)
-    `;
-
-    // Execute the SQL query
-    database.query(insertQuery, [firstName, lastName, passwordHash, email, dateOfBirth, nationality, userType], (error, results) => {
-      if (error) {
-        // Handle SQL error
-        console.log("SQL error", error);
-        return res.status(500).json({ message: "Error registering user", error: error.sqlMessage || error });
-      }
-      // Registration successful
-      console.log("Registration successful", results);
-      res.status(201).json({ message: "User registered successfully", userId: results.insertId });
+    // Create new user
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      passwordHash,
+      email,
+      dateOfBirth,
+      nationality,
+      userType
     });
+
+    console.log("Registration successful", newUser);
+    res.status(201).json({ message: "User registered successfully", userId: newUser.id });
   } catch (error) {
-    // Handle server error
-    console.log("Server error", error);
+    console.error("Registration error:", error);
     res.status(500).json({ message: "Server error", error: error.message || error });
   }
 };
