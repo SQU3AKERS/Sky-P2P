9f6e84d783601ca87887bdad6d0d74628f18a114
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-21 20:51:01 +0800

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,29 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ userType }) => {
+  // Function to generate navigation based on userType
+  const renderNavLinks = () => {
+    switch (userType) {
+      case 'borrower':
+        return (
+          <>
+            {/* Borrower-specific links */}
+            <Link to="/contracts">Contract +</Link>
+            <Link to="/rewards">Rewards +</Link>
+            {/* Common links */}
+            <Link to="/settings">Settings</Link>
+          </>
+        );
+      case 'lender':
+        return (
+          <>
+            {/* Lender-specific links */}
+            <Link to="/portfolio">Portfolio</Link>
+            {/* Common links */}
+            <Link to="/settings">Settings</Link>
+          </>
+        );
+      default:
+        // Links to show when no user is logged in
+        return (
+          <>
+            <Link to="/about-us">About us</Link>
+            <Link to="/blockchain">Blockchain +</Link>
+          </>
+        );
+    }
+  };
+
   return (
     <header className="header">
       <nav className="navbar">
+        {/* Logo and title always show */}
         <Link to="/" className="navbar-logo">
-          <img src="logo512.png" alt="Logo" />
+          <img src="/path-to-512logo.png" alt="Logo" />
           Sky's P2P Lending Platform
         </Link>
+        {/* Render links based on user type */}
         <div className="navbar-menu">
           <Link to="/home">Home</Link>
-          <Link to="/about-us">About us</Link>
-          <div className="dropdown">
-            <button className="dropbtn">Blockchain +</button>
-            <div className="dropdown-content">
-              <Link to="/transactions">Transactions</Link>
-              <Link to="/credit-scores">Credit Scores</Link>
-            </div>
-          </div>
+          {renderNavLinks()}
           <Link to="/support">Support</Link>
         </div>
+        {/* Right side of the navbar */}
         <div className="navbar-auth">
-          <Link to="/login" className="btn btn-login">Login</Link>
-          <Link to="/register" className="btn btn-signup">Sign Up</Link>
+          {userType ? (
+            <Link to="/logout" className="btn btn-logout">Logout</Link>
+          ) : (
+            <>
+              <Link to="/login" className="btn btn-login">Login</Link>
+              <Link to="/signup" className="btn btn-signup">Sign Up</Link>
+            </>
+          )}
         </div>
       </nav>
     </header>
