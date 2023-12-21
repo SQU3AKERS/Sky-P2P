ed6f0249a74fa10f1dd858db209e0ab7684415fe
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2023-12-21 22:37:59 +0800

    Update registerRoute.js
    Added logging

--- backend/routes/registerRoute.js
+++ backend/routes/registerRoute.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController');
 
-router.post('/register', registerController.registerUser);
+// router.post('/register', registerController.registerUser);
+router.post('/register', (req, res) => {
+    console.log('Register request received with data:', req.body);
+    registerController.registerUser(req, res);
+});
 
 module.exports = router;
