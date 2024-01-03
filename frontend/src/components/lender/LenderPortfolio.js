dca427a0dc606bd1c09ac4f77b1dc068b4aa696b
SQU3AKERS <114267818+SQU3AKERS@users.noreply.github.com>
2024-01-04 04:49:23 +0800

    Moved future enhancements to deprecated folder

---
+++ frontend/src/components/lender/LenderPortfolio.js
@@ -0,0 +1,11 @@
+<div className="user-main-menu">
+  <div className="menu-row top">
+    <div className="menu-item">Total Contracts Created</div>
+    <div className="menu-item">Total Contracts Funded</div>
+    <div className="menu-item">Credit Score</div>
+  </div>
+  <div className="menu-row bottom">
+    <div className="menu-item">Total Outstanding</div>
+    <div className="menu-item">Total Paid</div>
+  </div>
+</div>
